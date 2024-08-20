8edf4d90fe6b1c3c48710af2e81b87a024c9167d
Clowisen <87909204+clowisen@users.noreply.github.com>
2024-08-20 20:41:58 +0300

    Update password-strength-checker.js

--- scripts/password-strength-checker.js
+++ scripts/password-strength-checker.js
@@ -1,12 +1,12 @@
-$(document).ready(function(){
+$(document).ready(function(){ // Aşağıdaki kodların sayfa yüklendiği zaman çalışmasını sağlar
     const checkpoints = [false,false,false,false,false]; // Güçlü bir parolanın sağlaması gereken 5 şart
     $('#password').on('input', function(){  // #password isimli input kutusunda bir değişiklik yapıldığı zaman bu fonksiyonun çalışmasını sağlar
-        const password = $('#password').val(); // oluşturduğumuz password değişkenine #password inputundan gelen değeri atıyoruz
+        const password = $('#password').val(); // oluşturulan password değişkenine #password inputundan gelen değer atanıyor
         if(password.match(/[a-z]/g)){ 
             checkpoints[0] = true  // eğer alfabedeki küçük harflerden herhangi birisi girilirse birinci şartı sağlıyor
         }
         else{
-            checkpoints[0] = false // eğer sağlamıyorsa da false yapıyoruz ki input kutusundan değer silinirse barın uzunluğu gerilesin
+            checkpoints[0] = false // eğer sağlamıyorsa da false yapılmasını sağlıyoruz ki input kutusundan değer silinirse barın uzunluğu gerilesin
         }
         if(password.match(/[A-Z]/g)){ 
             checkpoints[1] = true // eğer alfabedeki herhangi bir büyük harf girilirse ikinci şartı sağlıyor
@@ -15,13 +15,13 @@ $(document).ready(function(){
             checkpoints[1] = false 
         }
         if(password.match(/[!,#,$,%,&,?]/g)){
-            checkpoints[2] = true // eğer [...] içerisindeki işaretlerden herhangi biri girilirse üçüncü şart sağlanıyor
+            checkpoints[2] = true // eğer yukarıda belirttiğimiz işaretlerden herhangi biri girilirse üçüncü şart sağlanıyor
         }
         else{
             checkpoints[2] = false
         }
         if(password.match(/[0-9]/)){
-            checkpoints[3] = true // eğer bir harf girilirse dördüncü şart da sağlanıyor
+            checkpoints[3] = true // eğer bir harf girilirse dördüncü şart sağlanıyor
         }
         else{
             checkpoints[3] = false
@@ -46,7 +46,7 @@ $(document).ready(function(){
             case 1:
                 $('#colorBar').css('background-color', 'red'); // eğer bir şartı sağlıyorsa barın rengi kırmızı oluyor ve alt satırdaki kod ile barın uzunluğu arttırılıyor
                 $('#colorBar').css('width', '10%');
-                break; // bir şart sağlandıysa gereken ayarlamalar yapılıyor ve bu satırdan sonraki kodların okunmasına gerek kalınmadığı için break ile durduruluyor
+                break; // bir şart sağlandıysa gereken ayarlamalar yapılıyor ve bu satırdan sonraki kodları okumasına gerek kalmadığı için break ile durduruluyor
             case 2:
                 $('#colorBar').css('background-color', 'yellow');
                 $('#colorBar').css('width', '25%');
